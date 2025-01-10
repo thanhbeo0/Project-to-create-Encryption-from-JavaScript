1efd75ca9f2557cbeaa546a799deac2d35323002
thanhbeo0 <lyvanthan16049@gmail.com>
2025-01-10 23:11:42 +0700

    Update binary.js

--- JavaScript/binary.js
+++ JavaScript/binary.js
@@ -4,4 +4,10 @@ function binary(ipt=""){
   }).join(' ');
 }
 
-export {binary};
+function binary4(ipt=""){
+  return ipt.split("").map(v=>{
+    return v.charCodeAt(0).toString(2).padStart(4,'0');
+  }).join(' ');
+}
+
+export {binary, binary4};
