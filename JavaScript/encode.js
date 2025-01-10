ada65a6a8019112dbb0a60420d1c252fbca1c3ee
thanhbeo0 <lyvanthan16049@gmail.com>
2025-01-10 23:11:00 +0700

    Update encode.js

--- JavaScript/encode.js
+++ JavaScript/encode.js
@@ -1,5 +1,7 @@
 export {block} from "./block.js";
 export {unblock} from "./unblock.js";
-export {binary} from "./binary.js";
+export {binary, binary4} from "./binary.js";
 export {unbinary} from "./unbinary.js";
 export {swappos} from "./swappos.js";
+export {hex16} from "./hex16.js";
+export {unhex16} from "./unhex16.js";
