779c3dad67229d6797536adad2e4f3268c06fdda
thanhbeo0 <lyvanthan16049@gmail.com>
2025-01-10 14:32:39 +0700

    Create encode.js

---
+++ JavaScript/encode.js
@@ -0,0 +1,5 @@
+export {block} from "./block.js";
+export {unblock} from "./unblock.js";
+export {binary} from "./binary.js";
+export {unbinary} from "./unbinary.js";
+export {swappos} from "./swappos.js";
